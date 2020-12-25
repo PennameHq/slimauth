cebba86b855e9e35856a6a4eb2af956a4052e183
Lincoln W Daniel <lwdthe1@lincolns-mbp.lan>
2020-12-24 21:12:55 -0500

    add SlimAuth file

---
+++ SlimAuth.js
@@ -0,0 +1,9 @@
+const CookieSessionManager = require('./CookieSessionManager')
+
+class SlimAuth {
+	createCookieSessionManager(config) {
+		return new CookieSessionManager(config)
+	}
+}
+
+module.exports = SlimAuth
